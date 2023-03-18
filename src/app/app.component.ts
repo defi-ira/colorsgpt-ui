28f13859cc760d492d272b4c86aca4c399a9475f
Ira <ira@agoras.app>
2023-03-17 23:04:44 -0400

    add color generation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,66 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ColorsGPT';
+    title = 'ColorsGPT';
+
+    public input: string;
+    public isLoading: boolean = false;
+
+    public colors: Color[] = [];
+
+    constructor() {
+        this.input = '';
+    }
+
+    public async getColors() {
+        this.isLoading = true;
+        console.log(this.input);
+        let resp = await this.callColorsFunction();
+        const colors = resp.colors;
+        if (colors) {
+            colors.forEach((color: string) => {
+                this.colors.push(new Color(color));
+            });
+        }
+        console.log(colors);
+        this.isLoading = false;
+    }
+
+    public async callColorsFunction() {
+        const requestOptions = {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ vibe: this.input })
+        };
+        const response = await fetch(environment.AZURE_FUNC_URL, requestOptions);
+        return response.json();
+    }
+
+    async copyToClipboard(text: string): Promise<void> {
+        try {
+          await navigator.clipboard.writeText(text);
+          console.log('Text copied to clipboard:', text);
+        } catch (err) {
+          console.error('Failed to copy text:', err);
+        }
+      }
+}
+
+export class Color {
+    public hex: string;
+    constructor(hex: string) {
+        this.hex = hex;
+    }
+
+    public getHex() {
+        return "#" + this.hex;
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatRippleModule } from '@angular/material/core';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { FormsModule } from '@angular/forms';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatCardModule,
+    MatRippleModule,
+    MatProgressSpinnerModule,
+    FormsModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
